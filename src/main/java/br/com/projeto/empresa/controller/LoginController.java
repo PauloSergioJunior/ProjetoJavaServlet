dfdce5db07a016f70c815091897ed271e693fc0b
Paulo Sergio <47927915+PauloSergioJunior@users.noreply.github.com>
2020-03-24 17:06:52 -0300

    Add files via upload

---
+++ src/main/java/br/com/projeto/empresa/controller/LoginController.java
@@ -0,0 +1,54 @@
+package br.com.projeto.empresa.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		PrintWriter out = response.getWriter();
+		
+		if(email.equals("paulo@gmail.com") && senha.equals("123")) {
+			
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+			
+		}else {
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			request.setAttribute("erro", "Login ou Senha incorretos!");
+			rd.forward(request, response);
+		}
+	}
+
+}
